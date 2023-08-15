4055f07e41aa9770fadd4c1cc4c9dc70c106cc1a
Byte717 <115853289+Byte717@users.noreply.github.com>
2023-08-15 09:56:37 -0400

    Added template
    Added basic python template

---
+++ template.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy
+import os
+import matplotlib.pyplot as plt
+import argparse
+from typing import *
+
+
+def main() -> int:
+    return 0
+
+
+if __name__ == '__main__':
+    res : int = main()
+    print(f"Code executed with exit code {res}")
