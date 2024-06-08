40c3f772daa3c3636dbe61c7e6ff5b3218cda8b8
Dhairya Sarvaiya <115853289+Byte717@users.noreply.github.com>
2024-06-08 09:00:02 -0400

    Update template.py

--- template.py
+++ template.py
@@ -5,6 +5,122 @@ import matplotlib.pyplot as plt
 import argparse
 from typing import *
 
+class TemplateClass:
+    def __init__(self, *args, **kwargs):
+        # Constructor
+        pass
+
+    def __str__(self):
+        # String representation
+        pass
+
+    def __repr__(self):
+        # Official string representation
+        pass
+
+    def __del__(self):
+        # Destructor
+        pass
+
+    def __len__(self):
+        # Length
+        pass
+
+    def __getitem__(self, key):
+        # Get item
+        pass
+
+    def __setitem__(self, key, value):
+        # Set item
+        pass
+
+    def __delitem__(self, key):
+        # Delete item
+        pass
+
+    def __iter__(self):
+        # Iterator
+        pass
+
+    def __next__(self):
+        # Next item in iteration
+        pass
+
+    def __contains__(self, item):
+        # Contains
+        pass
+
+    def __call__(self, *args, **kwargs):
+        # Callable
+        pass
+
+    def __enter__(self):
+        # Entering a context
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Exiting a context
+        pass
+
+    def __add__(self, other):
+        # Addition
+        pass
+
+    def __sub__(self, other):
+        # Subtraction
+        pass
+
+    def __mul__(self, other):
+        # Multiplication
+        pass
+
+    def __truediv__(self, other):
+        # True division
+        pass
+
+    def __floordiv__(self, other):
+        # Floor division
+        pass
+
+    def __mod__(self, other):
+        # Modulus
+        pass
+
+    def __pow__(self, other, modulo=None):
+        # Exponentiation
+        pass
+
+    def __eq__(self, other):
+        # Equality
+        pass
+
+    def __ne__(self, other):
+        # Not equal
+        pass
+
+    def __lt__(self, other):
+        # Less than
+        pass
+
+    def __le__(self, other):
+        # Less than or equal
+        pass
+
+    def __gt__(self, other):
+        # Greater than
+        pass
+
+    def __ge__(self, other):
+        # Greater than or equal
+        pass
+
+    def __hash__(self):
+        # Hash value
+        pass
+
+    def __bool__(self):
+        # Boolean value
+        pass
 
 def main() -> int:
     return 0
